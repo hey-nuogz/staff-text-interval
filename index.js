5d7db16285b04648ce9cb8774a3f45e0247593e1
Nuogz <zheung@vip.qq.com>
2022-03-29 00:08:00 +0800

    v1.5.0 use `@hey/staff-wock` to connect `hey`

--- index.js
+++ index.js
@@ -1,59 +1,32 @@
 import './lib/init.js';
 
 import PKG from './lib/global/package.js';
-import Wock from './lib/Wock.js';
-
 import C from './lib/global/config.js';
 import G from './lib/global/log.js';
 
 
-if(!~~C.interval || ~~C.interval < 1000) { throw Error('间隔无效或小于一秒'); }
+import StaffWock from '@hey/staff-wock';
 
-
-const wock = new Wock(new URL('wock', `http://${C.target.host}:${C.target.port}`).toString().replace(/^http/, 'ws'));
-
-
-
-const hey = push => wock.cast('hey/push', push);
-
-const run = async () => {
-	hey({ title: C.title, body: C.body });
-
+const staff = () => {
 	G.debug('主线', '定期~[通知]', '✔ ');
+
+	return { title: C.title, body: C.body };
 };
 
 
-
-let intervalRun;
-
-wock.add('start', () => {
-	run();
-
-	intervalRun = setInterval(run, C.interval);
-});
-
-wock.add('stop', () => clearInterval(intervalRun));
+const wockStaff = new StaffWock(
+	C.target,
+	PKG.name, C.token,
+	C.interval, staff
+);
 
 
-wock.add('auth-failed', (error) => {
-	G.error('主线', '认证', `✖ ${error.message ?? error}`);
-});
+wockStaff.add('authed', () => G.info('主线', '认证', '✔ '));
+wockStaff.add('auth-failed', error => G.error('主线', '认证', `✖ ${error.message ?? error}`));
+wockStaff.add('start', () => G.info('主线', '开始 ~[Staff]', '✔ '));
+wockStaff.add('stop', () => G.info('主线', '停止 ~[Staff]', '✔ '));
+
+wockStaff.add('setToken', token => C.$.edit('token', () => token));
 
 
-const authStaff = () => {
-	if(C.token) {
-		wock.cast('profile/auth-staff', PKG.name, C.token);
-	}
-};
-
-wock.add('setToken', token => {
-	C.$.edit('token', () => token);
-
-	authStaff();
-});
-
-
-wock.reopen = authStaff;
-wock.at('open', authStaff, true);
-
-wock.open();
+wockStaff.open();
